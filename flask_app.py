fff98f7072e4fa15c00d99db0de9e3d48656d416
IlyaVirtyozzz <ilya.mayorov.2003@list.ru>
2019-04-26 13:08:32 +0300

    0.9

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request
-import logging
-import json
+from functions import *
+import logging, time, json, random
 
 app = Flask(__name__)
 
-logging.basicConfig(level=logging.INFO, filename='app.log',filemode='w',
+logging.basicConfig(level=logging.INFO, filename='/home/IlyaVirtyozzz/mysite/app.log',
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 
-with open("info.json", "r", ) as read_file:
+with open("/home/IlyaVirtyozzz/mysite/info.json", "r", ) as read_file:
     info_i = json.load(read_file)
 
 sessionStorage = {}
@@ -34,122 +34,169 @@ def handle_dialog(res, req):
 
     if req['session']['new']:
         res['response']['text'] = \
-            'Привет! Как я могу к вам обращаться?'
-        res['response']['tts'] = \
-            'Привет! Как я могу к вам обращаться?'
-        res['response']['buttons'].extend([
-            {
-                'title': 'На "Вы"',
-                'hide': True
-            },
-            {
-                'title': 'На "Ты"',
-                'hide': True
-            }
-        ])
+            'Привет! Здесь можно проверить свой IQ!'
+        res['response']['buttons'].extend(info_i["buttons"]['mainmenu'])
         sessionStorage[user_id] = {
-            'user_age': None,
-            'game_started': False,
-            'official_appeal': None
+            'steps': 0  # 0-главное меню, 1-помощь, 2-тест
         }
-
         return
-    if sessionStorage[user_id]['official_appeal'] is None:
-        if req['request']['nlu']["tokens"][1].lower() in [
-            'ты', 'вы'
-
-        ]:
-            if req['request']['nlu']["tokens"][1].lower() == 'ты':
-                sessionStorage[user_id]['official_appeal'] = False
-            if req['request']['nlu']["tokens"][1].lower() == 'вы':
-                sessionStorage[user_id]['official_appeal'] = True
-            res['response']['text'] = 'Приняла. А сколько {} лет? Если не секрет.'.format(
-                'вам' if sessionStorage[user_id]['official_appeal'] else 'тебе')
-
-            return
+    if sessionStorage[user_id]['steps'] == 0:
+        if 'помощь' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+            sessionStorage[user_id]['steps'] = 1  # переходим в "Помощь"
+            res['response']['text'] = 'Выберите что-хотите узнать'
+            res['response']['buttons'].extend(info_i["buttons"]['infomenu'])
+        elif 'начать' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+            sessionStorage[user_id]['steps'] = 2  # переходим в тестирование
+            sessionStorage[user_id]['user_age'] = None  # здесь будет хранится возраст юзера
+            sessionStorage[user_id]['attempt'] = 0  # вопрос 1
+            sessionStorage[user_id][
+                'start_test'] = False  # переходим в впорос но не начинаем его, так как нужно ещё спросить возраст
+            sessionStorage[user_id]['percent_true'] = None  # здесь будет храниться результат пользователя в процентах
+            sessionStorage[user_id]['iq'] = None  # IQ
+            sessionStorage[user_id]['answers'] = list(
+                map(lambda x: 0, range(60)))  # создаём список с 0, 0-неправильный ответ, 1-правильный
+            res['response']['text'] = 'Приняла. А сколько вам лет? Если не секрет.'
+        elif 'выйти' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+            res['end_session'] = True  # выходим
+            res['response']['text'] = 'Пока-пока'
         else:
             res['response']['text'] = \
-                'Не понимаю вас. Давайте ещё раз, но внятнее'
-            res['response']['buttons'].extend([
-                {
-                    'title': 'На "Вы"',
-                    'hide': True
-                },
-                {
-                    'title': 'На "Ты"',
-                    'hide': True
-                }
-            ])
-            return
-    if sessionStorage[user_id]['user_age'] is None:
-        age = get_age(req)
-        if age:
-            sessionStorage[user_id]['user_age'] = age[0]
-            res['response']['text'] = 'Приступим к тестированию?'
-            res['response']['buttons'].extend([
-                {
-                    'title': "Да",
-                    'hide': True
-                },
-                {
-                    'title': 'Нет',
-                    'hide': True
-                }
-            ])
-        else:
-            res['response']['text'] = 'Не поняла. Повторите'
+                'Не понимаю вас. Давайте ещё раз, но внятнее'  # ¯\_(ツ)_/¯
+            res['response']['buttons'].extend(info_i["buttons"]['mainmenu'])
         return
 
+    if sessionStorage[user_id]['steps'] == 1:
+        info_run(res, req)
+    if sessionStorage[user_id]['steps'] == 2:
+        test_start(res, req)
+
+
+def info_run(res, req):
+    user_id = req['session']['user_id']
+    res['response']['buttons'].extend(info_i["buttons"]['infomenu'])
+    if 'краткая' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+        res['response'][
+            'text'] = 'Тест Рейвена включает в себя 60 впросов. Время на решение заданий ограничено 20 минутами.' \
+                      ' Расчитан для людей возрастом от 8 до 65 лет. Все ответы приблизительны.'
+    elif 'назад' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+        sessionStorage[user_id]['steps'] = 0  # обратно в меню
+        res['response']['buttons'] = info_i["buttons"]['mainmenu']
+        res['response']['text'] = 'Привет! Здесь можно проверить свой IQ!'
+    elif 'википедии' in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+        res['response']['text'] = "Открываю"  # открываем страницу на википедии
     else:
-        if not sessionStorage[user_id]['game_started']:
-            if 'да' in req['request']['nlu']['tokens']:
-                sessionStorage[user_id]['game_started'] = True
-                sessionStorage[user_id]['attempt'] = 0
-                test_start(res, req)
-            elif 'нет' in req['request']['nlu']['tokens']:
-                res['response']['text'] = 'Ну и ладно!'
-                res['end_session'] = True
-            else:
-                res['response']['text'] = 'Не поняла ответа! Так да или нет?'
-                res['response']['buttons'] = [
-                    {
-                        'title': 'Да',
-                        'hide': True
-                    },
-                    {
-                        'title': 'Нет',
-                        'hide': True
-                    }
-                ]
-        else:
-            test_start(res, req)
-
-
-def get_age(req):
-    age = []
-    for entity in req['request']['nlu']['entities']:
-        if entity['type'] == 'YANDEX.NUMBER':
-            age.append(entity['value'])
-
-    return age
+        res['response']['text'] = "Не понимаю вас"  # ¯\_(ツ)_/¯
 
 
 def test_start(res, req):
     user_id = req['session']['user_id']
     attempt = sessionStorage[user_id]['attempt']
-    if True:
-        res['response']['text'] = 'qwerty'
-        res['response']['card'] = {
-            "type": "BigImage",
-            "image_id": info_i["images"][
-                (len(info_i['images']) - attempt) % len(info_i['images'])],
-            "title": "Заголовок для изображения",
-            "description": "Описание изображения.",
-            "button": {
-                "text": "Надпись на кнопке",
-                "url": "http://example.com/",
-                "payload": {}
-            }}
+    start_test = sessionStorage[user_id]['start_test']
+
+    if sessionStorage[user_id]['user_age'] is None:
+        age = get_num(req)
+        if age and type(age) is int:
+            if 7 < age < 80:
+                sessionStorage[user_id]['user_age'] = str(age)
+                sessionStorage[user_id]['start_time'] = int(time.time())
+                sessionStorage[user_id]['start_test'] = True
+                res['response'][
+                    'text'] = 'Отправьте любое сообщение чтобы начать. У вас есть 20 минут чтобы ответить на вопросы'
+                res['response']['buttons'] = [{"title": "Любое сообщение.", "hide": True}]
+
+            elif not 7 < age < 80:
+                res['response']['text'] = 'Брррррррр,введите настоящий возраст'
+        else:
+            res['response']['text'] = 'Не поняла. Повторите'  # ¯\_(ツ)_/¯
+        return
+    else:
+        if start_test:
+            if attempt == 0:
+                sessionStorage[user_id]['start_time'] = int(time.time())  # засекаем время
+
+            if sessionStorage[user_id]['start_time'] - int(time.time() - 1200) < 0:
+                res['response']['text'] = 'Упс. Похоже время вышло. '
+                end_test(res, user_id)  # заканчиваем тест
+                return
+
+            if attempt != 0 and attempt < 60:
+                num = get_num(req)
+                if 1 <= int(num) <= 8:
+                    check_true(attempt, num, user_id)
+                else:
+                    if info_i["answers_info"][attempt][2] in 'AB':
+                        res['response']['buttons'].extend(info_i["buttons"]['AB'])
+                    else:
+                        res['response']['buttons'].extend(info_i["buttons"]['CDE'])
+                    res['response']['text'] = 'Введите корректный ответ'  # ¯\_(ツ)_/¯
+                    return
+
+            if attempt < 60:
+                sessionStorage[user_id]['attempt'] += 1
+                res['response']['text'] = 'qwerty'
+                res['response']['card'] = {
+                    "type": "BigImage",
+                    "title": "Вопрос: {}/60".format(attempt + 1),  # выводим позицию текущего вопроса
+                    "description": "{}".format(
+                        random.choice(info_i["dialogs"]["reassurance"]) + "\nОсталось времени: {}мин {}с".format(
+                            # время
+                            *check_time(sessionStorage[user_id]['start_time'] - int(time.time() - 1200)))),
+                    "image_id": info_i["answers_info"][
+                        (len(info_i["answers_info"]) + attempt) % len(info_i["answers_info"])][0],
+                }
+                # в заданиях группы А и Б по 6 возможных вариантов ответов , в С,Д,Е по 8
+                if info_i["answers_info"][attempt][2] in 'AB':
+
+                    res['response']['buttons'].extend(info_i["buttons"]['AB'])
+                else:
+                    res['response']['buttons'].extend(info_i["buttons"]['CDE'])
+            else:
+                res['response']['text'] = ''
+                end_test(res, user_id)
+
+
+        else:
+            res['response']['buttons'] = info_i["buttons"]['aftertest']  # меню после теста
+            res['response']['buttons'][0]['url'].format(sessionStorage[user_id]['iq'])
+            if 'узнать' in list(
+                    map(lambda x: x.lower(), req['request']['nlu']["tokens"])):  # узнать информацию о своём интеллекте
+                res['response'][
+                    'text'] = 'Открываю'
+            elif "меню" in list(map(lambda x: x.lower(), req['request']['nlu']["tokens"])):
+
+                sessionStorage[user_id]['steps'] = 0  # переходим в главное меню
+                res['response']['buttons'] = info_i["buttons"]['mainmenu']
+                res['response']['text'] = 'Привет! Здесь можно проверить свой IQ!'
+
+            else:
+                res['response']['text'] = "Не понимаю вас"  # ¯\_(ツ)_/¯
+
+
+def check_true(attempt, answer, user_id):
+    # проверка на правильность ответа
+    if answer == info_i["answers_info"][attempt - 1][1]:
+        sessionStorage[user_id]['answers'][attempt - 1] = 1
+    else:
+        pass
+
+
+def end_test(res, user_id):
+    sessionStorage[user_id]['start_test'] = False
+    sessionStorage[user_id]['percent_true'], sessionStorage[user_id]['iq'] = calculate_answer(user_id,
+                                                                                              sessionStorage,
+                                                                                              info_i)
+
+    res['response'][
+        'text'] += "Вы выполнили этот тест на {} процентов. Это означает что у вас {}." \
+                   " Ваш iq примерно составляет... {}. ".format(sessionStorage[user_id]['percent_true'],
+                                                                get_info_p(
+                                                                    sessionStorage[user_id]['percent_true']),
+                                                                sessionStorage[user_id]['iq'] if
+                                                                sessionStorage[user_id][
+                                                                    'iq'] != '-' else 'Упс... невозможно вычислить')
+    res['response']['buttons'] = info_i["buttons"]['aftertest']
+    res['response']['buttons'][0]['url'] = res['response']['buttons'][0]['url'].format(
+        sessionStorage[user_id]['iq'])
 
 
 if __name__ == '__main__':
---
+++ functions.py
@@ -0,0 +1,72 @@
+def calculate_answer(user_id, sessionStorage, info_i):
+    percent_iq = int(sessionStorage[user_id]['answers'].count(1) / 60 * 100)  # проценты правильных ответов
+    estimated_iq = None  # неточный IQ
+    # находим по таблице приблизительных значений
+    if 8 <= int(sessionStorage[user_id]['user_age']) <= 13:
+        estimated_iq = info_i["raven_diagnostics"]["table"][sessionStorage[user_id]['user_age']][
+            sessionStorage[user_id]['answers'].count(1)]
+    elif 14 <= int(sessionStorage[user_id]['user_age']) <= 30:
+        estimated_iq = info_i["raven_diagnostics"]["table"]['14-30'][sessionStorage[user_id]['answers'].count(1)]
+    else:
+        if 30 <= int(sessionStorage[user_id]['user_age']) <= 35:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 97) * 100
+        elif 36 <= int(sessionStorage[user_id]['user_age']) <= 40:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 93) * 100
+
+        elif 41 <= int(sessionStorage[user_id]['user_age']) <= 50:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 88) * 100
+
+        elif 51 <= int(sessionStorage[user_id]['user_age']) <= 55:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 82) * 100
+
+        elif 51 <= int(sessionStorage[user_id]['user_age']) <= 55:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 76) * 100
+
+        else:
+            estimated_iq = (int(info_i["raven_diagnostics"]["table"]['14-30'][
+                                    sessionStorage[user_id]['answers'].count(1)]) / 70) * 100
+    if estimated_iq == '-':
+        return (percent_iq, estimated_iq)
+    else:
+        return (percent_iq, int(estimated_iq))
+
+
+def get_info_p(percent):
+    percent = int(percent)
+    text = None
+    if percent <= 5:
+        text = 'дефектная интеллектуальная способность'
+    elif 5 <= percent <= 24:
+        text = 'интеллект ниже среднего.'
+    elif 25 <= percent <= 74:
+        text = 'средник интеллект'
+    elif 75 <= percent <= 95:
+        text = 'незаурядный интеллект'
+    elif 95 <= percent:
+        text = 'особо высокоразвитый интеллект'
+
+    return text
+
+
+def check_time(before_time_s):
+    # переводим в минуты и секунды
+    time_m = int(before_time_s // 60)
+    time_s = int(before_time_s % 60)
+    return time_m, time_s
+
+
+def get_num(req):
+    # находим число
+    age = []
+    for entity in req['request']['nlu']['entities']:
+        if entity['type'] == 'YANDEX.NUMBER':
+            age.append(entity['value'])
+
+    if len(age) != 0:
+        return age[0]
+    return None
